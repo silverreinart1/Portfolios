f9ec855b24713af0422bc3ea65709eb9a6676c7f
Silver Reinart <145542354+silverreinart1@users.noreply.github.com>
2024-12-12 08:40:49 +0200

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -16,9 +16,12 @@
       <h2 class="text-xl font-light uppercase tracking-wider mb-2">Silver Reinart</h2>
       <h3 class="text-2xl font-light leading-snug mb-4">Your Partner in Bringing Your Web Design Vision to Life</h3>
       <p class="mb-4 text-sm">
-        I am currently studying IT at Kuressaare Ametikool, where I am expanding my knowledge and skills in web development, design, 
-        and other cutting-edge technologies. My education gives me the foundation to deliver modern and effective solutions tailored 
-        to the needs of today's digital world.
+        Mina olen Silver Reinart, 17-aastane noormees, kes õpib Kuressaare Ametikoolis IT erialal. Olen omandanud mitmeid IT-alaseid oskusi, sealhulgas HTML, CSS, TailwindCSS, JavaScript ja PHP, ning kasutan neid teadmisi enesekindlalt. Mind köidab IT-valdkond, kuna see pakub võimalusi luua praktilisi ja innovaatilisi lahendusi, mis on kasulikud nii teistele kui ka mulle endale.
+          Lisaks õpingutele meeldib mulle arvutis aega veeta ja videomänge mängida. Videomängud on minu jaoks nii lõõgastus kui ka inspiratsioon. Vaba aega veedan ka jalgpalli mängides, et hoida end füüsiliselt aktiivsena ja mitmekülgsena. Need hobid aitavad mul tasakaalustada õpingutest ja igapäevaelust tulenevat stressi.
+          Iseloomult olen sõbralik, lahke ja tore. Hindan häid suhteid sõprade ja lähedastega ning usun, et need omadused aitavad mul luua tugevaid sidemeid nii isiklikus elus kui ka tulevases karjääris. Ma naudin suhtlemist ja usun, et avatus aitab kaasa edukale koostööle teistega.
+          Minu eesmärk on lühiajalises plaanis täiustada oma olemasolevaid IT-oskusi ja õppida uusi tehnoloogiaid, näiteks React ja Node.js. Pikaajalises plaanis soovin leida huvitava töökoha IT-sektoris või luua oma projekte, mis võiksid kasvada suuremateks ettevõtmisteks.
+          Minu jaoks on oluline hoida tasakaalu õpingute, hobide ja isikliku elu vahel. See aitab mul säilitada motivatsiooni ja arendada ennast nii vaimselt kui ka füüsiliselt. Pean oluliseks leida aeg nii tööle kui ka lõõgastumisele, et elu oleks mitmekülgne ja nauditav.
+          Olen kindel, et minu oskused, huvid ja iseloomuomadused annavad mulle võimaluse saavutada oma eesmärgid IT valdkonnas ning leida edu nii professionaalses kui ka isiklikus elus.
       </p>
 
 
